4b1b034a97d93c00f27a851c18fcb356f2a702f2
GaryKelly <C00207281@itcarlow.ie>
2018-12-10 11:00:51 +0000

    All headers added, functions need to be completed

--- Project1/Project1/Vector3.h
+++ Project1/Project1/Vector3.h
@@ -1,12 +1,34 @@
 #ifndef VECTOR3_H
 #define VECTOR3_H
 
-class Vector3
+#include <iostream>
+
+class MyVector3
 {
 public:
 
-private:
+	MyVector3();
+	MyVector3(double t_x, double t_y, double t_z);
+	double returnX();
+	void setX(double t_num);
+	double returnY();
+	void setY(double t_num);
+	double returnZ();
+	void setZ(double t_num);
+	double Length();
+	double LengthSquared();
+	void Normalise();
+	MyVector3 operator+(MyVector3 t_v1);
+	MyVector3 operator-(MyVector3 t_v1);
+	double operator*(MyVector3 t_v1);
+	MyVector3 operator*(const double t_num)const;
+	double operator^(const MyVector3 t_v1)const;
+	std::string toString();
 
+private:
+	double m_x;
+	double m_y;
+	double m_z;
 };
 
 
